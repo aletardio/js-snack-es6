592279cfd84276c38ae283a763d4f756b65522da
aletardio <ale.tardio99@gmail.com>
2023-12-06 15:53:22 +0100

    Added SNACK 1

--- js/script.js
+++ js/script.js
@@ -21,3 +21,18 @@ let bicycles = [
         weight: 17
     },
 ];
+
+// Dichiarazione di una variabile bici con peso minore con il peso massimo possibile
+let bikeLighterWeight = { weight: Infinity };
+
+// Ciclo forEach per cercare la bici con peso minore
+bicycles.forEach(elem => {
+    if(elem.weight < bikeLighterWeight.weight){
+        bikeLighterWeight = elem;
+    }
+});
+
+// Utilizzo della destructuring per ottenere nome e peso
+const { name, weight } = bikeLighterWeight;
+
+console.log(`La bici con il peso minore è ${name} con un peso di ${weight} kg.`);
