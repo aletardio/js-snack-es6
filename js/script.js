577c322b0b05fca3445bfc82e6a0d7b63f0d9c64
aletardio <ale.tardio99@gmail.com>
2023-12-06 15:06:21 +0100

    Created array of objects

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Creazione dell'array di oggetti
+let bicycles = [
+    {
+        name: 'BMX',
+        weight: 8
+    },
+    {
+        name: 'Graziella',
+        weight: 10
+    },
+    {
+        name: 'EcoPedal',
+        weight: 15
+    },
+    {
+        name: 'Bici da Corsa',
+        weight: 7
+    },
+    {
+        name: 'AvanzoRide',
+        weight: 17
+    },
+];
